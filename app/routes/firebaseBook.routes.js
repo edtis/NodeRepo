4c65eada48e8522cbdcc2069ddd2a1cf94587c3b
ranjeet <ranjeet.kumar@tothenew.com>
2020-08-17 08:48:22 +0530

    added global config file

--- app/routes/firebaseBook.routes.js
+++ app/routes/firebaseBook.routes.js
@@ -3,9 +3,7 @@ module.exports = app => {
   // Add headers
   app.use(function(req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader("Access-Control-Allow-Origin", [
-      "https://goodbookbible.study"
-    ]);
+    res.setHeader("Access-Control-Allow-Origin", [allowURL]);
 
     // Request methods you wish to allow
     res.setHeader(
