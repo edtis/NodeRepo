c9858b0a74976ae5b51599b52bae36d8a3be6632
Ranjeet Kumar <ranjeetk3>
2021-11-02 09:37:46 +0530

    modified cors

--- config/global.config.js
+++ config/global.config.js
@@ -1,5 +1,11 @@
 const allowURL = [
   "https://goodbookbible.study",
+  "https://goodbookbible.study/",
+  "https://www.goodbookbible.study",
+  "https://www.goodbookbible.study/",
   "https://goodbookbible.social",
+  "https://goodbookbible.social/",
+  "https://www.goodbookbible.social",
+  "https://www.goodbookbible.social/",
 ];
 module.exports = allowURL;
